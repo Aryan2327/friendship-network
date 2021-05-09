ab7ac743c1522537b3ba6f4dcdf16b7784d1a64f
Kiana Williams <kwill113@binghamton.edu>
2021-05-09 16:52:12 -0400

    update files

---
+++ UserClass.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+#include "UserClass.h"
+
+using namespace std;
+
+UserClass::UserClass(){
+//stores new user information
+}
+
+UserClass::User(string n){
+
+}
+
+		
+void UserClass::setName(string n){
+	saved_n = n;
+
+}
+		
+
+
+string UserClass::getName(){
+	return saved_n;	
+
+}
+
+void UserClass::createFriends(){
+
+}
+
+void UserClass::addFriends(){
+
+}
+		
