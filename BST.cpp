8a40f99db6bd1c205fe4803ca31230161dc42972
Aryan Chaudhary <achaudh9@cs.binghamton.edu>
2021-05-09 18:30:02 -0400

    bst insert

--- BST.cpp
+++ BST.cpp
@@ -11,3 +11,21 @@ template <typename T>
 BST<T>::BST(){
     root = nullptr;
 }
+
+template <typename T>
+void BST<T>::insert(T key){
+    insertHelper(key, root);
+}
+
+template <typename T>
+void BST<T>::insertHelper(T key, Node* node){
+    if (node == nullptr){
+        node = new Node(key);
+    }
+    else if (key < node->key){
+        insertHelper(key, node->left);
+    }
+    else if (key > root->key){
+        insertHelper(key, node->right);
+    }
+}
