d602e6250587bd663594ee10ce207f4983f91950
cchoi20 <cchoi20@binghamton.edu>
2021-05-13 16:05:03 -0400

    CA5 heap class

--- Heap.cpp
+++ Heap.cpp
@@ -41,7 +41,7 @@ void Heap::swap(int pos1, int pos2){
 }
 void Heap::increaseKey(int i) {
 	list[i].addListen();
-	cout << list[i].getListens() << endl;
+	cout << list[i].getTitle() << " " << list[i].getListens() << endl;
 	/*while(i != 0 && list[parent(i)].getListens() < list[i].getListens()){
 		int temp = i;
 		swap(temp, parent(temp));
