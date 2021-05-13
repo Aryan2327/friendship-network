b302b324d25cd46ea60385ba299d90a8a36823a0
cchoi20 <cchoi20@binghamton.edu>
2021-05-13 15:31:14 -0400

    CA5 heap class

--- HeapTest.cpp
+++ HeapTest.cpp
@@ -12,7 +12,13 @@ int main(int argc, char *argv[]){
 	heap.insert(s2);
 	heap.insert(s3);
 	heap.insert(s4);
+	s5.addListen();
 	heap.insert(s5);
+	heap.print();
+	heap.increaseKey(s4);
+	heap.print();
+	heap.increaseKey(s4);
+	heap.increaseKey(s3);
 	heap.print();
 	/*heap.increaseKey(4);
 	cout << "increased index 4" << endl;
